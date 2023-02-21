8f7c8bd04b02cdcef6bcaf6d176100152ee726ec
Emitify <okerekemmanuel7@gmail.com>
2023-02-21 11:38:43 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * *print_alphabet - print all alphabet in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= '2'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
+
